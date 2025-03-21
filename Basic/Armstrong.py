bec8b85dccc71e2c706726670560cc0481053aa0
dineshkanna <dineshkanna654@gmail.com>
2025-03-21 23:18:58 +0530

    `Added reverse_string function in Basic/one.py`

---
+++ Basic/Armstrong.py
@@ -0,0 +1,16 @@
+
+def Arm(n):
+    num_list = [int(num)**3 for num in str(n)]
+    sum = 0
+    print(num_list)
+    for i in num_list:
+        sum = sum + i
+    if sum == n:
+        return f"This is Armstrong Number"
+    else:
+        return "Not a Armstrong"
+
+
+a = Arm(153)
+
+print(a)
--- Basic/one.py
+++ Basic/one.py
@@ -1,5 +1,7 @@
-"""Write a function that reverses a string without using built-in reverse functions. 
-    Example: 'hello' → 'olleh'"""
+"""
+Write a function that reverses a string without using built-in reverse functions. 
+    Example: 'hello' → 'olleh'
+"""
 
 def reverse_string(s):
     return s[::-1]
