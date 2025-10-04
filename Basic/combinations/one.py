d23d1bfb5b9f06954d95c4416bc9b7221cc33359
dineshkanna <dineshkanna654@gmail.com>
2025-10-04 17:59:36 +0530

    latest

---
+++ Basic/combinations/one.py
@@ -0,0 +1,10 @@
+# Count total pairs
+# Write code to count how many 2-element combinations can be made from a given list.
+# Brute force solution
+a = [1, 2, 3]
+
+for i in range(len(a)):
+	print("*")
+	for j in range(i+1, len(a)):
+		#print(a[i], a[j])
+		pass
