09f03e96d933aac57fb7a1fe2d460829f70b324a
dineshkanna <dineshkanna654@gmail.com>
2025-10-06 23:59:31 +0530

    latest

--- Basic/combinations/one.py
+++ Basic/combinations/one.py
@@ -1,10 +1,35 @@
 # Count total pairs
 # Write code to count how many 2-element combinations can be made from a given list.
 # Brute force solution
-a = [1, 2, 3]
+#a = [1, 2, 3]
 
-for i in range(len(a)):
-	print("*")
-	for j in range(i+1, len(a)):
-		#print(a[i], a[j])
-		pass
+
+def main(a):
+	pairs = []
+	for i in range(len(a)):
+		for j in range(i+1, len(a)):
+			pairs.append((a[i], a[j]))
+	return len(pairs)
+
+# print(main([1, 2, 3, 4]))
+
+def fun2(a):
+	n = len(a)
+	combination_count = (n * (n - 1)/2)
+	return combination_count
+
+#print(fun2([1, 2, 3, 4]))
+
+# Find pairs with difference = 3
+# a = [1, 4, 7, 9, 11, 13]
+
+def fun3(a):
+    pairs = []
+    for i in range(len(a)):
+        for j in range(i + 1, len(a)):
+            if a[j] - a[i] == 3:
+                pairs.append((a[i], a[j]))
+    return pairs
+
+print(fun3([1, 4, 7, 9, 11, 13]))
+
---
+++ Medium/combination/main.py
@@ -0,0 +1,32 @@
+# Print unique pairs whose product is even
+# Example: [2, 3, 4, 5, 6]
+# ➤ Output: (2,3), (2,4), (2,5), (2,6), (3,4), (3,6), (4,5), (4,6), (5,6)
+
+def fun1(a):
+	pairs = []
+	for i in range(len(a)):
+		for j in range(i+1, len(a)):
+			if ((a[i] * a[j]) % 2 == 0):
+				pairs.append((a[i], a[j]))
+	return pairs
+
+# print(fun1([2, 3, 4, 5, 6]))
+
+# Find the pair with the maximum sum
+# Input: [1, 5, 7, 9, 2, 8]
+# ➤ Output: (9, 8)
+
+def fun2(a):
+	all_pairs = []
+	all_sum = []
+	for i in range(len(a)):
+		for j in range(i+1, len(a)):
+			all_pairs.append((a[i], a[j]))
+	for pair in all_pairs:
+		all_sum.append(sum(pair))
+	max_sum = max(all_sum)
+	max_index = all_sum.index(max_sum)
+	max_sum_pair = all_pairs[max_index]
+	return max_sum_pair
+
+print(fun2([1, 5, 7, 9, 2, 8]))
