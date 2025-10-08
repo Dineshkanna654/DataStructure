8cbdbced95c82135c149676d6177ca736a4baaaa
dineshkanna <dineshkanna654@gmail.com>
2025-10-08 23:24:13 +0530

    latest

--- Hard/combination/main.py
+++ Hard/combination/main.py
@@ -32,10 +32,45 @@ def fun2(a):
 			indices_of_integers.append(i)
 	for index_of_integer in indices_of_integers:
 		result.append(all_pairs[index_of_integer])
-	print("all_pairs:", all_pairs)
-	print("all_sum", all_sum)
-	print("indices_of_integers", indices_of_integers)
-	print("result", result)
+#	print("all_pairs:", all_pairs)
+#	print("all_sum", all_sum)
+#	print("indices_of_integers", indices_of_integers)
+#	print("result", result)
 	# return result
 
-print(fun2([1, 2, 3, 4, 5, 6]))
+# print(fun2([1, 2, 3, 4, 5, 6]))
+
+
+# Find the closest pair
+
+# Input: [10, 13, 17, 21, 25]
+# ➤ Output: (10, 13) because difference = 3, the smallest.
+
+def fun3(a):
+	dics = {}
+	diffs = []
+	for i in range(len(a)):
+		for j in range(i+1, len(a)):
+			dics[f"({a[i]}, {a[j]})"] = a[j] - a[i]
+	for key, val in dics.items():
+		diffs.append(val)
+		min_val = min(diffs)
+		if val == min_val:
+			return key
+
+# print(fun3([10, 13, 17, 21, 25]))
+
+# Generate all unique team matchups
+
+# Input: teams = ["A", "B", "C", "D"]
+teams = ["A", "B", "C", "D"]
+
+def fun4(a):
+	combs = []
+	for i in range(len(a)):
+		for j in range(i+1, len(a)):
+			combs.append((a[i], a[j]))
+	for comb in combs:
+		print(comb)
+
+print(fun4(teams))
