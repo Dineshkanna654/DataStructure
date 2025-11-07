904401f3a3026d638da1a200bcd538efa36882e0
dineshkanna <dineshkanna654@gmail.com>
2025-11-07 14:05:06 +0530

    latest

--- Hard/longest-common-prefix.py
+++ Hard/longest-common-prefix.py
@@ -1,22 +1,30 @@
-class Solution(object):
-    def longestCommonPrefix(self, strs):
-        """
-        :type strs: List[str]
-        :rtype: str
-        """
-        if not strs:
-            return ""
-        prefix = strs[0]
-        result = ''
+# Write a function to find the longest common prefix string amongst an array of strings.
 
-        for i in range(len(strs)):
-            for s in strs[1:]:
-                if s[i] != prefix[i]:
-                    return result
-            result += prefix[i]
-        return result
-    
-# Example usage:
-sol = Solution()
-print(sol.longestCommonPrefix(["flower","flow","flight"]))  # Output: "fl
-print(sol.longestCommonPrefix(["dog","racecar","car"]))     # Output: ""
+# If there is no common prefix, return an empty string "".
+
+# Example 1:
+# Input: strs = ["flower","flow","flight"]
+# Output: "fl"
+
+# Example 2:
+# Input: strs = ["dog","racecar","car"]
+# Output: ""
+# Explanation: There is no common prefix among the input strings.
+
+def longestPrefix(strs):
+	if not strs:
+		return ""
+
+	prefix = strs[0]
+	result = ""
+
+	for i in range(len(prefix)):
+		for s in strs[1:]:
+			if i >= len(s) or prefix[i] != s[i]:
+				return result
+		result += prefix[i]
+	return result
+
+
+print(longestPrefix(["flower", "flow", "flight"]))
+print(longestPrefix(["dog", "racecar", "car"]))
